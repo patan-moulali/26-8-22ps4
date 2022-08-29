6b386a25ff36531814ea6480f7b1cd7e0e5d2ee7
Patan Moulali <patan.moulali@mphasis.com>
2022-08-29 22:45:05 +0530

    Add project files.

---
+++ 26-8-22ps4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_8_22ps4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> mylist = new LinkedList<string>();
+            mylist.AddFirst("jack");
+            mylist.AddLast("veena");
+            mylist.AddAfter(mylist.First, "jill");
+            mylist.AddBefore(mylist.Last, "jim");
+            mylist.AddBefore(mylist.Last.Previous, "pari");
+            mylist.AddAfter(mylist.First.Next, "hari");
+            LinkedListNode<string> foundnode = mylist.Find("jim");
+            foundnode.Value = "don";
+
+            foreach (var item in mylist)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
